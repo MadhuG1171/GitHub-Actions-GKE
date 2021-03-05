32bc701031efbd91ccc6a9c7d6c5766f9ebb9797
unknown <Administrator@BG4WS0860.SONATA.LOCAL>
2021-03-05 14:39:38 +0530

    INitial commit for GKE

---
+++ src/main/java/com/sonata/GitHubActionsDemo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.sonata.GitHubActionsDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
